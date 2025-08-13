04d9ca00a18b26546ccf8e6ff873ef09675ddd14
sharapovm479 <Muzaffar.Sharapov@techconsulting.tech>
2025-08-13 12:49:16 -0400

    complete a1

--- iOSBasicsLearnings.playground/Contents.swift
+++ iOSBasicsLearnings.playground/Contents.swift
@@ -1,3 +1,54 @@
 import UIKit
+import SwiftUI
+import PlaygroundSupport
 
-var greeting = "Hello, playground"
+//var greeting = "Hello, playground"
+
+//Assignment 1
+//In Swift playgorund have multiple examples to show usage of below:
+//1.Constants
+//2.Variables
+//3.Type inferincing
+//4.Explicit Inference
+//5.What all data types we have in swift
+//
+//Tomorrow in classroom you need to explain your written code, so dont rush in finishing the assignment, take your time and do it throughly.
+//Once Completed this and push to code to githib and share link of it in this group itself.@Joshua Moutray Sulouff II @MuzaffarSharapov
+
+let name = "Zaff"
+let age = 99
+let isStudent = true
+let height = 1.78
+let weight: Float = 170.0000
+
+
+let nameE: String = "Zaff"
+let ageE: Int = 99
+let isStudentE: Bool = true
+let heightE: Double = 1.78
+let weightE: Float = 70.0
+
+
+
+
+////expl
+//var str: String = "string"
+//var int: Int = 1
+//var bool: Bool = true
+//var double: Double = 1.0
+//var float: Float = 1.0
+//var array: [String] = ["a", "b", "c"]
+//var dict: [String: Int] = ["a": 1, "b": 2]
+//var dictArr: [String:[Int]] = ["a": [1,2], "b": [3,4]]
+//var optional: String? = "optional"
+//var optionalInt: Int? = 1
+//var optionalBool: Bool? = true
+//var optionalDouble: Double? = 1.0
+//var optionalFloat: Float? = 1.0
+//var optionalArray: [String]? = ["a", "b", "c"]
+//var optionalDict: [String: Int]? = ["a": 1, "b": 2]
+
+
+
+
+
